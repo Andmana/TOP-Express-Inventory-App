37f1253af8da948dbf2292eff679a0a81a9882f8
andmana <iam.andmana@gmail.com>
2025-05-24 20:14:52 +0700

    Create dotenv for dev

--- db/pool.js
+++ db/pool.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 // Destructure and check required environment variables
 const { DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD } =
   process.env;
+const IS_DEV = process.env.NODE_ENV === "dev";
 
 // Validate environment variables
 if (!DATABASE_HOST || !DATABASE_NAME || !DATABASE_USER || !DATABASE_PASSWORD) {
@@ -15,7 +16,10 @@ export default new Pool({
   user: DATABASE_USER,
   database: DATABASE_NAME,
   password: DATABASE_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false, // Add this for non-production environments like local development
-  },
+  port: 5432,
+  ssl: IS_DEV
+    ? false
+    : {
+        rejectUnauthorized: false, // Add this for non-production environments like local development
+      },
 });
