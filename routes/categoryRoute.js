42c279a524ac8e5a5dd1e17eee05becdf89bff41
andmana <iam.andmana@gmail.com>
2025-05-26 23:24:53 +0700

    Install multer for image upload

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
 import categoryController from "../controllers/categoryController.js";
+import multer from "multer";
+
 const router = express.Router();
+const upload = multer({ dest: "public/icons/uploads/categories/" });
 
 router.get("/", categoryController.getAllCategories);
 router.get("/create", categoryController.getCreate);
-router.post("/create", categoryController.postCreate);
+router.post("/create", upload.single("icon"), categoryController.postCreate);
 
 export default router;
