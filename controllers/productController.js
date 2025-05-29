1efc4bdcbb70a83b3f685c996948d3a4058c8ec3
andmana <iam.andmana@gmail.com>
2025-05-29 22:05:32 +0700

    Create sidebar product views

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,10 @@
 import productRepository from "../repositories/productRepository.js";
+import categoryRepository from "../repositories/categoryRepository.js";
 
 const getAllProducts = async (req, res, next) => {
   const products = await productRepository.getAllProducts();
-  res.render("product/index", { products });
+  const categories = await categoryRepository.getAllCategories();
+  res.render("product/index", { products, categories });
 };
 
 export default { getAllProducts };
