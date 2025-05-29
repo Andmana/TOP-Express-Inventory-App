7f5fc7c1725662a97fc6b1fbe735d76617f9a960
andmana <iam.andmana@gmail.com>
2025-05-29 22:53:28 +0700

    Refactor product sidebar

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,11 @@
 import productRepository from "../repositories/productRepository.js";
 import categoryRepository from "../repositories/categoryRepository.js";
+import getContrastColor from "../utils/contrastColors.js";
 
 const getAllProducts = async (req, res, next) => {
   const products = await productRepository.getAllProducts();
   const categories = await categoryRepository.getAllCategories();
-  res.render("product/index", { products, categories });
+  res.render("product/index", { products, categories, getContrastColor });
 };
 
 export default { getAllProducts };
