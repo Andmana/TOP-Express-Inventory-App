1fee289ee0c83c2d67468d6b06460aaee580125b
andmana <iam.andmana@gmail.com>
2025-05-30 13:25:00 +0700

    Add query get product by id

--- repositories/productRepository.js
+++ repositories/productRepository.js
@@ -34,4 +34,26 @@ async function getAllProducts(
   return rows;
 }
 
-export default { getAllProducts };
+async function getProductById(id) {
+  const { rows } = await pool.query(`
+    SELECT 
+      p.id AS id,
+      p.name AS name,
+      p.description,
+      p.price,
+      p.quantity,
+      p.brand,
+      p.icon_src,
+      c.id AS category_id,
+      c.name AS category_name,
+      c.color AS category_color,
+      c.icon_src AS category_icon
+    FROM products p
+    JOIN categories c ON p.category_id = c.id
+    WHERE p.id = ${id}
+  `);
+
+  return rows[0] || null;
+}
+
+export default { getAllProducts, getProductById };
